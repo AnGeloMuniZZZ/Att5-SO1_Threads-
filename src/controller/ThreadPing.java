511d79e4c2ddbfe022e6b33c1f408340ac84d669
angel <angel@DESKTOP-S83E2U3>
2024-03-14 11:37:16 -0300

    Primerio commit

---
+++ src/controller/ThreadPing.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ThreadPing extends Thread{
+	private String process;
+	private int i;
+	
+	public ThreadPing(String process, int i) {
+		this.process = "ping -4 -c 10 " + process;
+		this.i = i;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Process p = Runtime.getRuntime().exec(process);
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine().concat(process);
+			linha = buffer.readLine();
+			linha = buffer.readLine();
+			
+			while (linha != null) {
+				if (linha.contains("time=")) {
+					String[] split = linha.split("time=");
+					switch (i) {
+						case 1:
+							System.out.println("UOL tempo: " + split[1]);
+							break;
+						case 2:
+							System.out.println("Terra tempo: " + split[1]);
+							break;
+						case 3:
+							System.out.println("Google tempo: " + split[1]);
+							break;
+					}
+				} synchronized (ThreadPing.class) {
+					if (linha.contains("rtt")) {
+						String split[] = linha.split("/");
+						switch (i) {
+						case 1:
+							System.out.println("UOL média --> " + split[4]);
+							break;
+						case 2:
+							System.out.println("Terra média --> " + split[4]);
+							break;
+						case 3:
+							System.out.println("Google média --> " + split[4]);
+							break;
+						}
+					}
+				}
+				linha = buffer.readLine();
+			}
+			//System.out.println(bufferPing);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
